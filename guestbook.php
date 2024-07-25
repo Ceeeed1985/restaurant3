0d25a292396da79185a4eea15a581abb0bbaf311
Cédric Truyen <cedrictruyen@icloud.com>
2024-07-25 17:11:49 +0200

    avancement sur la mise des éléments guestbook sur le site web

--- guestbook.php
+++ guestbook.php
@@ -1,5 +1,21 @@
+<?php
+
+try{
+$bdd = new PDO('mysql:host=localhost;dbname=restaurant2.0;charset=utf8','root','');
+} catch(Exception $e){
+    die("Erreur : ".$e->getMessage());
+}
+
+$motif3 = "guestbook";
+$statut = "online";
+
+$requeteGuestbook = $bdd->prepare('SELECT *
+                                    FROM messagerie
+                                    WHERE motif = ? AND status = ?');
+$requeteGuestbook->execute(array($motif3, $statut));
 
 
+?>
 
 
 <!DOCTYPE html>
@@ -89,28 +105,25 @@
         </div>
     </div>
 
-
-    <div class="container-fluid py-4 background-guestbook">
-      <div class="container">
-        <div class="row d-flex">
-          <div class="col-md-12 pb-4 text-center">
-            <h2 class="display-4">Livre <span class="text-warning">d'or</span></h2>
-          </div>
-          <div class="col-md-12 pb-4 text-center">
-            <h2 class="display-4">Livre <span class="text-warning">d'or</span></h2>
-          </div>
-          <div class="col-md-12 pb-4 text-center">
-            <h2 class="display-4">Livre <span class="text-warning">d'or</span></h2>
-          </div>
-          <div class="col-md-12 pb-4 text-center">
-            <h2 class="display-4">Livre <span class="text-warning">d'or</span></h2>
-          </div>
-          <div class="col-md-12 pb-4 text-center">
-            <h2 class="display-4">Livre <span class="text-warning">d'or</span></h2>
-          </div>
-          <section class="col-md-6">
-                
-          </section>
+    <section class="container d-flex justify-content-center flex-wrap">
+        <?php
+            while ($donnees = $requeteGuestbook->fetch()){
+                echo '
+                    <section class="col-md-5 mx-2 border border-1 rounded bg-warning bg-opacity-10 py-3 px-4 my-2 d-flex">
+                        <section class="col-md-2 d-flex justify-content-start align-items-center">
+                            <p style="font-size: 5em;" class="text-warning">&laquo;</p>
+                        </section>
+                        <section class="col-md-8 d-flex justify-content-center align-items-center flex-column">
+                            <h6>De '.$donnees['prenom'].' '.$donnees['nom'].'</h6>
+                            <p><em>'.$donnees['message'].'</em></p>
+                        </section>
+                        <section class="col-md-2 d-flex justify-content-end align-items-center">
+                            <p style="font-size: 5em;" class="text-warning">&raquo;</p>
+                        </section>
+                    </section>';
+                    }
+            ?>
+    </section>
 
 
 
