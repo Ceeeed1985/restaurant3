d7edda751aac3165601c09418432e1a811c39d3d
Cedric Truyen <cedrictruyen@icloud.com>
2024-08-28 11:18:50 +0200

    ok pour la page guestbook

--- controller/controller.php
+++ controller/controller.php
@@ -1,8 +1,14 @@
 <?php
 
     require('model/contactModel.php');
+    require('model/guestbookModel.php');
 
     function getContactView() {
         $requete = postMessage();
         require('view/contactView.php');
+    }
+
+    function getGuestbookView() {
+        $requeteGuestbook = getGuestMessages();
+        require('view/guestbookView.php');
     }
--- index.php
+++ index.php
@@ -3,9 +3,13 @@
     require('controller/controller.php');
 
     try {
+
       if(isset($_GET['page'])){
+        
         if($_GET['page'] == 'contact'){
           getContactView();
+        } else if($_GET['page'] == 'guestbook'){
+          getGuestbookView();
         } else {
           throw new Exception ("Cette page n'existe pas ou a été supprimée");
         }
